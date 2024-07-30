418adb289025f4ec54f2e3dad66b84667c21db79
Shreeram <shreeram1162004@gmail.com>
2024-07-30 17:07:46 +0530

    push to deploy

--- index.js
+++ index.js
@@ -9,31 +9,24 @@ import { dirname } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
+
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
-
-
-const port = 3000;
-
-
-
-
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
  const ApiEndpt="https://www.googleapis.com/books/v1/volumes?q=";
 
  const API_KEY =process.env.GOOGLE_BOOKS_API_KEY;
  
  
 
-app.use(express.static("public"));
+
  app.get("/",(req,res)=>{
 const quotes=movieQuotes.random();
-    res.render("start.ejs",{quotes:quotes})
+    res.render("start",{quotes:quotes})
  });
 
 app.post("/search", async (req,res)=>{
@@ -42,9 +35,9 @@ app.post("/search", async (req,res)=>{
   const response = await axios.get(`${ApiEndpt}${title}&maxResults=40`);
   const result=response.data;
  
-res.render("result.ejs",{title:title,result:result})
+res.render("result",{title:title,result:result})
    }catch(error){
-res.render("result.ejs",{title: "no match found",result:[]})
+res.render("result",{title: "no match found",result:[]})
 res.status(503).send("503 too many request 😣😣😣😣")
    }
 
@@ -55,11 +48,11 @@ res.status(503).send("503 too many request 😣😣😣😣")
       const response2= await axios.get( `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&orderBy=relevance&maxResults=15&orderBy=newest&key=${API_KEY}`);
       const result1=response1.data;
       const result2=response2.data;
-res.render("home.ejs",{result1:result1,result2:result2})   ;    
+res.render("home",{result1:result1,result2:result2})   ;    
    }
    catch(error){
       console.error('Error fetching trending books:', error);
-      res.render("home.ejs",{result:[]})
+      res.render("home",{result:[]})
       res.status(503).send("503 too many request 😣😣😣😣")
    }
  })
@@ -69,7 +62,7 @@ res.render("home.ejs",{result1:result1,result2:result2})   ;
     const text=req.query.textSnippet;  
    const response= await axios.get( `${selfLink}`)
       const result=response.data;
-   res.render("item.ejs",{result:result,text:text});
+   res.render("item",{result:result,text:text});
    }catch(error){
       res.status(503).send("503 too many request 😣😣😣😣")
    }
@@ -80,11 +73,11 @@ res.render("home.ejs",{result1:result1,result2:result2})   ;
 const category=req.body.id;
 const response = await axios.get(`${ApiEndpt}subject:${category}&maxResults=40`);
 const result=response.data;
-res.render("result.ejs",{title:category,result:result});
+res.render("result",{title:category,result:result});
 
 
    }catch(error){
-res.render("result.ejs",{title: "no match found",result:[]})
+res.render("result",{title: "no match found",result:[]})
 res.status(503).send("503 too many request 😣😣😣😣")
    }
 
